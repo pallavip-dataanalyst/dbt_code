e090210d92d41538d2694dc360cc9ead591bf8c1
pallavip.dataanalyst@gmail.com <pallavip.dataanalyst@gmail.com>
2024-08-29 23:29:27 +0000

    commit

--- models/orders.sql
+++ models/orders.sql
@@ -6,7 +6,8 @@ WITH tb1  as(
  select
         id,
         order_date,
-        user_id
+        user_id,
+        status
         
      from {{source('datafeed_shared_schema','STG_ORDERS')}})
      select * from tb1
